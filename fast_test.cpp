47ea64bcdf8e1fb1635cfcf82d0cc86a7be4efb6
Giuseppe Cesarano <pecorainpannacotta@gmail.com>
2023-05-28 11:46:06 +0200

    added small test

---
+++ fast_test.cpp
@@ -0,0 +1,21 @@
+#include "newtpp.hpp"
+#include <iostream>
+
+int main()
+{
+  newt::root_window::init();
+
+  /* Positioning is relative to the last window created *
+   * in win costructor it's relative to the root window */
+  newt::window win { newt::usize { 50, 8 } }; 
+
+  // Now is relative to win
+  newt::label text { "Hello, could you please press ok?", { 10, 1 } };
+  newt::button ok_button { "ok", { 22, 3 } };
+
+  newt::form simple_message { text, ok_button };
+
+  simple_message.run();
+
+  newt::root_window::finish();
+}
